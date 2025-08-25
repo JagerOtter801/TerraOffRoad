f554248790652a74c1b2e7264cbee682386e0d1b
JAYTHINKPAD14S\jason <jason.e.green@outlook.com>
2025-08-25 15:39:00 -0600

    HotFix: Updated Hamburger Location. IOS had a very unattractive location for hamburger.

--- screens/MapsScreen.tsx
+++ screens/MapsScreen.tsx
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from "react-native-safe-area-context";
 import MapTabScreen from "./MapTabScreen";
 import RoutesTabScreen from "./RoutesTabScreen";
 import UserProfileTabScreen from "./UserProfileTabScreen";
@@ -16,55 +16,63 @@ const Tab = createBottomTabNavigator();
 
 const MapsScreen = ({ user }: MapsScreenProps) => {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarStyle: { ...styles.maps_bottom_tab_navigation },
-          tabBarActiveTintColor: "#E5E2E1",
-          tabBarInactiveTintColor: "#9E9998",
-          tabBarBackground: () => null,
-        }}
-      >
-        <Tab.Screen
-          name="Map"
-          component={MapTabScreen}
-          options={{
-            headerShown: false,
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="map" size={size} color={color} />
-            ),
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarStyle: { ...styles.maps_bottom_tab_navigation },
+            tabBarActiveTintColor: "#E5E2E1",
+            tabBarInactiveTintColor: "#9E9998",
+            tabBarBackground: () => null,
           }}
-        />
-        <Tab.Screen
-          name="Routes"
-          component={RoutesTabScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="directions" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Offline Maps"
-          component={OfflineMapsScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="cloud-download" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={() => <UserProfileTabScreen />}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="account-circle" size={size} color={color} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+        >
+          <Tab.Screen
+            name="Map"
+            component={MapTabScreen}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <MaterialIcons name="map" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Routes"
+            component={RoutesTabScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <MaterialIcons name="directions" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Offline Maps"
+            component={OfflineMapsScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <MaterialIcons
+                  name="cloud-download"
+                  size={size}
+                  color={color}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={() => <UserProfileTabScreen />}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <MaterialIcons
+                  name="account-circle"
+                  size={size}
+                  color={color}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 };
