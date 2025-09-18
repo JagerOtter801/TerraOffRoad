ebf650ba4810adb8f368c4a4b1f286484bd1bdfd
JAYTHINKPAD14S\jason <jason.e.green@outlook.com>
2025-09-18 15:57:53 -0600

    - Added missing dependency for weather

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -4,6 +4,7 @@ import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { styles } from "../../styles";
 import { gpsService, Waypoint, Route, Coordinate } from "../navigation";
 import { MapLongPressEvent } from "../navigation/types";
+import { testWeatherApi } from "../Weather/WeatherReport";
 import { useEffect, useState, useRef } from "react";
 import { useNavigation, DrawerActions } from "@react-navigation/native";
 
