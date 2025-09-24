0d0e52f8bcfaea35f047b12be87b4b5fec9b98e5
JAYTHINKPAD14S\jason <jason.e.green@outlook.com>
2025-09-24 17:20:58 -0600

    - Updated ALL translations

--- src/appNavigation/MapTabDrawerScreen.tsx
+++ src/appNavigation/MapTabDrawerScreen.tsx
@@ -5,23 +5,26 @@ import ProfileStack from "./ProfileStack"
 import HelpScreen from "../screens/HelpScreen";
 import ContactUsScreen from "../screens/ContactUsScreen";
 import SettingsScreen from "../screens/SettingsScreen";
+import { useTranslation } from 'react-i18next';
 
 const Drawer = createDrawerNavigator();
 
+
 const MapTabDrawerScreen = () => {
+  const {t}= useTranslation();
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
       <Drawer.Navigator>
         <Drawer.Screen
-          name="MainTabs"
+          name={t("main menu")}
           component={TabsNavigator}
           options={{ headerShown: false }}
           key="main-tabs"
         />
-        <Drawer.Screen name="Profile" component={ProfileStack} />
-        <Drawer.Screen name="Settings" component={SettingsScreen} />
-        <Drawer.Screen name="Help / FAQs" component={HelpScreen} />
-        <Drawer.Screen name="Contact Us" component={ContactUsScreen} />
+        <Drawer.Screen name={t("profile")} component={ProfileStack} />
+        <Drawer.Screen name={t("settings")} component={SettingsScreen} />
+        <Drawer.Screen name={t("help / faqs")} component={HelpScreen} />
+        <Drawer.Screen name={t("contact us")} component={ContactUsScreen} />
       </Drawer.Navigator>
     </SafeAreaView>
   );
